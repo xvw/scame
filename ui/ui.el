ba0787ad5f6e7d4acc9a1d10135a84c5bce2b837
xvw <xavier.vdw@gmail.com>
2024-09-04 15:30:53 +0200

    Go for spacemacs-theme (but load sanityc-tomorrow)

--- ui/ui.el
+++ ui/ui.el
@@ -3,7 +3,18 @@
 
 (load "ui/preset")
 
-(use-package gruvbox-theme
+(use-package color-theme-sanityinc-tomorrow
+  :init (progn (load-theme 'sanityinc-tomorrow-day t t)
+	       (load-theme 'sanityinc-tomorrow-night t t)
+	       (load-theme 'sanityinc-tomorrow-blue t t)
+	       (load-theme 'sanityinc-tomorrow-bright t t)
+	       (load-theme 'sanityinc-tomorrow-eighties t t))
+  :defer t
   :ensure t)
 
-(load-theme 'gruvbox-dark-hard t)
+(use-package spacemacs-theme
+  :init (progn (load-theme 'spacemacs-dark t t)
+	       (load-theme 'spacemacs-light t t)
+               (enable-theme 'spacemacs-dark))
+  :defer t
+  :ensure t)
