9d64ad36bf88eafb8c319c2a3626809f0f687bbd
xvw <xavier.vdw@gmail.com>
2024-10-10 14:34:49 +0200

    Introduce a very minimal rust suppport

--- lang/lang.el
+++ lang/lang.el
@@ -1,5 +1,6 @@
 (load "lang/markdown")          ;; Support for editing Markdown
 (load "lang/yaml")              ;; Support for editing Yaml files
 (load "lang/web")               ;; Global support for web edition
+(load "lang/rust")              ;; Minimal support for Rust edition
 (load "lang/ocaml/ocaml")    ;; Support for the OCaml language with baremetal Merlin
 ;; (load "lang/ocaml-eglot/ocaml") ;; Support for the OCaml language with eglot and OCaml-lsp
