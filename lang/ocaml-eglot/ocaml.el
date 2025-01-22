e8e54929b55038a9c5a66f7b9f49464e8143e993
xvw <xavier.vdw@gmail.com>
2025-01-22 11:14:48 +0100

    Tweak

--- lang/ocaml-eglot/ocaml.el
+++ lang/ocaml-eglot/ocaml.el
@@ -4,4 +4,3 @@
 (add-hook #'tuareg-mode-hook #'ocaml-eglot)
 (add-hook #'ocaml-eglot-hook #'eglot-ensure)
 (add-hook #'after-save-hook #'eglot-format)
-(add-hook 'eglot-managed-mode-hook (lambda () (eglot-inlay-hints-mode -1))) 
