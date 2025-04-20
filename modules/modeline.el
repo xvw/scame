75f9a9f5bd238f469092d2534e1aff06747439c3
xvw <xavier.vdw@gmail.com>
2025-04-20 17:22:21 +0200

    More modeline customisation

--- modules/modeline.el
+++ modules/modeline.el
@@ -2,6 +2,12 @@
 (use-package doom-modeline
   :ensure t
   :config
-  (setq doom-modeline-buffer-file-name-style 'relative-from-project)
+  (setq doom-modeline-buffer-file-name-style 'relative-from-project
+        doom-modeline-support-imenu t
+        doom-modeline-height 25
+        doom-modeline-icon t
+        doom-modeline-buffer-state-icon t
+        doom-modeline-buffer-modification-icon t
+        doom-modeline-lsp-icon t)
   :init
   (doom-modeline-mode))
