172476a4253af7d2423efee47922e19582ffbe9a
xvw <xavier.vdw@gmail.com>
2026-02-15 15:06:09 +0100

    Move to Neocaml and Prettify symbol

--- lang/ocaml/neocaml.el
+++ lang/ocaml/neocaml.el
@@ -1,7 +1,9 @@
 (use-package neocaml
   :ensure t
+  :hook
+  (neocaml-mode . prettify-symbols-mode)
   :config
-  ;; Register neocaml modes with Eglot
+  ;; Register neocaml modes with Eglot-Managed-Mode-Hook
   (with-eval-after-load 'eglot
     (add-to-list 'eglot-server-programs
                  '((neocaml-mode neocaml-interface-mode) . ("ocamllsp")))))
