c6fe775f75f37e7c50c89d0acbdd91447c5e11b5
xvw <xavier.vdw@gmail.com>
2026-07-15 09:00:00 +0200

    Neocaml

--- lang/ocaml/ocaml.el
+++ lang/ocaml/ocaml.el
@@ -1,8 +1,8 @@
 ;; -*- lexical-binding: t; -*-
 
 (load "lang/ocaml/transient")
-;; (load "lang/ocaml/neocaml")
-(load "lang/ocaml/tuareg")
+(load "lang/ocaml/neocaml")
+;; (load "lang/ocaml/tuareg")
 
 (defun xvw-ocaml-eglot-setup ()
   "My custom OCaml-eglot Setup"
@@ -12,25 +12,11 @@
 (global-set-key (kbd "C-c b") #'ocaml-build)
 (global-set-key (kbd "M-RET") #'recompile)
 
-;; (use-package ocaml-eglot
-;;   :ensure t
-;;   :after neocaml
-;;   :hook
-;;   (neocaml-base-mode . ocaml-eglot-mode)
-;;   (ocaml-eglot-mode . eglot-ensure)
-;;   (ocaml-eglot-mode . xvw-ocaml-eglot-setup)
-;;   :config
-;;   (setq ocaml-eglot-syntax-checker 'flymake)
-;;   (set-face-foreground 'eglot-diagnostic-tag-unnecessary-face "gold3")
-;;   (set-face-background 'eglot-highlight-symbol-face "dark slate blue")
-;;   (set-face-foreground 'eglot-highlight-symbol-face "plum"))
-
-
 (use-package ocaml-eglot
   :ensure t
-  :after tuareg
+  :after neocaml
   :hook
-  (tuareg-mode . ocaml-eglot-mode)
+  (neocaml-base-mode . ocaml-eglot-mode)
   (ocaml-eglot-mode . eglot-ensure)
   (ocaml-eglot-mode . xvw-ocaml-eglot-setup)
   :config
@@ -39,13 +25,26 @@
   (set-face-background 'eglot-highlight-symbol-face "dark slate blue")
   (set-face-foreground 'eglot-highlight-symbol-face "plum"))
 
+
+;; (use-package ocaml-eglot
+;;   :ensure t
+;;   :after tuareg
+;;   :hook
+;;   (tuareg-mode . ocaml-eglot-mode)
+;;   (ocaml-eglot-mode . eglot-ensure)
+;;   (ocaml-eglot-mode . xvw-ocaml-eglot-setup)
+;;   :config
+;;   (setq ocaml-eglot-syntax-checker 'flymake)
+;;   (set-face-foreground 'eglot-diagnostic-tag-unnecessary-face "gold3")
+;;   (set-face-background 'eglot-highlight-symbol-face "dark slate blue")
+;;   (set-face-foreground 'eglot-highlight-symbol-face "plum"))
+
 (use-package opam-switch-mode
   :ensure t
   :hook
-  (tuareg-mode . opam-switch-mode)
-  ;; (neocaml-mode . opam-switch-mode)
-  ;; (neocaml-interface-mode . opam-switch-mode)
-  )
+  ;; (tuareg-mode . opam-switch-mode)
+  (neocaml-mode . opam-switch-mode)
+  (neocaml-interface-mode . opam-switch-mode))
 
 (use-package ocp-indent
   :ensure t
